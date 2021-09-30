72cdbf1a3f3298f411040cbb4675fa905cdc7bfb
Marco Zanzottera <marcozanzottera@e.email>
2021-09-30 21:29:42 +0200

    Capitolo 2 Esercizio 4

---
+++ Capitolo 2/Esercizi/4/4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2, n3;
+    float f1, f2, f3;
+
+    printf("n1: %d\nn2: %d\nn3: %d\n", n1, n2, n3);
+    printf("f1: %f\nf2: %f\nf3: %f\n", f1, f2, f3);
+
+    return 0;
+}
