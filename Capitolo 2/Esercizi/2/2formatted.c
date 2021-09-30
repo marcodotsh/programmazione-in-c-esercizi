be4386b22c40a78fcdd033825c20fcb75e5412e3
Marco Zanzottera <marcozanzottera@e.email>
2021-09-30 15:17:23 +0200

    Inserimento zip con programmi del libro

---
+++ Capitolo 2/Esercizi/2/2formatted.c
@@ -0,0 +1,11 @@
+/*Direttive*/
+#include <stdio.h>
+
+int main(void)
+{
+    /*Istruzioni*/
+    printf("Parkinsons Law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+}
