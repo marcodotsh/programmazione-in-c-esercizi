26e3608fb54b1edd29ed7da88295b95e11625aa5
Marco Zanzottera <marcozanzottera@e.email>
2021-11-27 13:05:23 +0100

    Esercitazione 15

---
+++ Esercitazioni/15/es4/malloc2d.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *malloc2d(int r, int c);
+
+int main(void) {
+	int rig, col, *mat, i, j;
+	
+	printf("Inserisci numero righe: ");
+	scanf("%d", &rig);
+	printf("Inserisci numero colonne: ");
+	scanf("%d", &col);
+
+	mat = malloc2d(rig, col);
+	if(mat) {
+		for(i=0;i<rig;i++) {
+			for(j=0;j<col;j++) {
+				printf("%d\t", *(mat+i*col+j));	
+			}
+			printf("\n");
+		}
+	}
+	else
+		printf("Errore allocazione memoria\n");
+ 
+	return 0;
+}
+
+int *malloc2d(int r, int c)
+{
+	int *p, i, j;
+	p = malloc(sizeof(int)*r*c);
+	if(p) {
+		for(i=0;i<r;i++) {
+			for(j=0;j<c;j++) {
+				*(p+i*c+j) = 0;
+			}
+		}
+	}
+
+	return p;
+}
